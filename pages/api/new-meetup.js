d5fec83087795d1530fa5af7cbc3aa904b2c588b
Anant <anantpatel07012003@gmail.com>
2023-07-18 19:12:40 +0530

    data received in database

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -5,18 +5,21 @@ async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
     // body is basically the content to the request,the body of the request or basically the data
-    const { title, adress, description, image } = data;
+    // const { title, adress, description, image } = data;
     const client = await MongoClient.connect(
-      "mongodb+srv://Anant:This_IsAwesome@cluster0.grjntze.mongodb.net/meetups?retryWrites=true&w=majority"
+      "mongodb+srv://Anant:This_IsAwesome@cluster0.grjntze.mongodb.net/?retryWrites=true&w=majority"
     );
     const db = client.db();
-    const meetupCollection = db.collection("meetups");
-    const result = await meetupCollection.insertOne();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
     // insertOne is the method to insert one new document into the  collection
     console.log(result);
+
     client.close();
 
-    res.status(201).json({ message: "meetup inserted" });
+    res.status(201).json({ message: "Meetup inserted!" });
   }
 }
 export default handler;
--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,8 +1,19 @@
 import NewMeetUpForm from "../../components/meetups/NewMeetupForm";
+import { useRouter } from "next/router";
 function NewMeetUpPage() {
-  const onAddMeetupHandler = (enteredValue) => {
-    console.log(enteredValue);
-  };
+  const router = useRouter();
+  async function onAddMeetupHandler(enteredValue) {
+    const response = await fetch("/api/new-meetup", {
+      method: "POST",
+      body: JSON.stringify(enteredValue),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+    router.push("/");
+  }
   return <NewMeetUpForm onAddMeetup={onAddMeetupHandler} />;
 }
 export default NewMeetUpPage;
