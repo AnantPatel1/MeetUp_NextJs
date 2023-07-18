aad9f002276b61b9af8ccad8422e9946df4d9195
Anant <anantpatel07012003@gmail.com>
2023-07-18 19:33:20 +0530

    getting data from mongo db

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,30 @@
 import MeetupList from "../components/meetups/MeetupList";
-
-const Dummy_items = [
-  {
-    id: "m1",
-    title: "Rivers and Moutains",
-    address: "Switzerland, 1243 ",
-    image: "https://nitsaholidays.in/uploads/blog/588661nitsa.jpg",
-    description: "Beauty",
-  },
-  {
-    id: "m2",
-    title: "Skyscrappers",
-    address: "Chicago, 1245 ",
-    image:
-      "https://t4.ftcdn.net/jpg/02/20/54/73/360_F_220547309_vGPvZ7CEp09UOtRw3QIsmQaRS0zgQWjB.jpg",
-    description: "Awesome",
-  },
-  {
-    id: "m3",
-    title: "Historical Buildings",
-    address: "Croatia, 1246 ",
-    image:
-      "https://media.cnn.com/api/v1/images/stellar/prod/230113155058-01-istria-kvarner-croatia-rab.jpg?c=original",
-    description: "Beauty",
-  },
-];
+import { MongoClient } from "mongodb";
+// const Dummy_items = [
+//   {
+//     id: "m1",
+//     title: "Rivers and Moutains",
+//     address: "Switzerland, 1243 ",
+//     image: "https://nitsaholidays.in/uploads/blog/588661nitsa.jpg",
+//     description: "Beauty",
+//   },
+//   {
+//     id: "m2",
+//     title: "Skyscrappers",
+//     address: "Chicago, 1245 ",
+//     image:
+//       "https://t4.ftcdn.net/jpg/02/20/54/73/360_F_220547309_vGPvZ7CEp09UOtRw3QIsmQaRS0zgQWjB.jpg",
+//     description: "Awesome",
+//   },
+//   {
+//     id: "m3",
+//     title: "Historical Buildings",
+//     address: "Croatia, 1246 ",
+//     image:
+//       "https://media.cnn.com/api/v1/images/stellar/prod/230113155058-01-istria-kvarner-croatia-rab.jpg?c=original",
+//     description: "Beauty",
+//   },
+// ];
 
 function HomePage(props) {
   // const [loadMeetup, setLoadMeetup] = useState([]);
@@ -37,28 +37,43 @@ function HomePage(props) {
   return <MeetupList meetups={props.meetups} />;
 }
 
-export async function getServerSideProps(context) {
-  const req = context.req;
-  const res = context.res;
-  // The req object contains information about the incoming request, such as the HTTP method, the URL, and the cookies.
-  // The res object represents the outgoing response, and it can be used to set the response headers, the status code, and the body of the response.
+// export async function getServerSideProps(context) {
+//   const req = context.req;
+//   const res = context.res;
+// The req object contains information about the incoming request, such as the HTTP method, the URL, and the cookies.
+// The res object represents the outgoing response, and it can be used to set the response headers, the status code, and the body of the response.
 
-  // fetch data from an API
-  return {
-    props: {
-      meetups: Dummy_items,
-    },
-  };
-}
-
-// export async function getStaticProps() {
-//   // fetch data from an API
+// fetch data from an API
 //   return {
 //     props: {
 //       meetups: Dummy_items,
 //     },
-//     revalidate: 1,
 //   };
 // }
+
+export async function getStaticProps() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://Anant:This_IsAwesome@cluster0.grjntze.mongodb.net/?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+  const meetuplist = await meetupsCollection.find().toArray();
+  // since it returns a promise await it
+  client.close();
+
+  return {
+    props: {
+      meetups: meetuplist.map((meetup) => ({
+        title: meetup.title,
+        image: meetup.image,
+        address: meetup.address,
+        description: meetup.description,
+        id: meetup._id.toString(),
+      })),
+    },
+    revalidate: 1,
+  };
+}
 export default HomePage;
 // remember to use serverside props only when your data changes more frequently so that revalidate cannot help you, or you need acees to request and response, in every other cases we should use only staticprops
