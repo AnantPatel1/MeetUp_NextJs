4b2d18cfa983f479f9e28607224bb64405383fc4
Anant <anantpatel07012003@gmail.com>
2023-07-18 16:28:09 +0530

    server side props

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,38 @@ const Dummy_items = [
   },
 ];
 
-function HomePage() {
-  return <MeetupList meetups={Dummy_items} />;
+function HomePage(props) {
+  // const [loadMeetup, setLoadMeetup] = useState([]);
+  // if you visit the index.html in dev tools, you will find the <ul> to be empty, this is because the NextJs does not wait for the next render cycle
+  // here initially the loadmeetup is empty [], so this is hown in the dev tools
+  // useEffect(() => {
+  //   // send a http request to fetch data
+  //   setLoadMeetup(Dummy_items);
+  // }, []);
+  return <MeetupList meetups={props.meetups} />;
 }
+
+export async function getServerSideProps(context) {
+  const req = context.req;
+  const res = context.res;
+  // The req object contains information about the incoming request, such as the HTTP method, the URL, and the cookies.
+  // The res object represents the outgoing response, and it can be used to set the response headers, the status code, and the body of the response.
+
+  // fetch data from an API
+  return {
+    props: {
+      meetups: Dummy_items,
+    },
+  };
+}
+
+// export async function getStaticProps() {
+//   // fetch data from an API
+//   return {
+//     props: {
+//       meetups: Dummy_items,
+//     },
+//     revalidate: 1,
+//   };
+// }
 export default HomePage;
